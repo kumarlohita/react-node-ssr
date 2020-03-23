5648759a883781599cc31d6b243c5d099bc10b57
kumarlohita <kumarlohita1987@gmail.com>
2020-03-23 16:25:43 -0400

    handled error with json on CSR

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,14 @@
 import { GET } from "./APIMethods";
+import { PRODUCTS } from "../constants/products";
 
 export const FETCH_PRODUCTS = async () => {
   const url = "http://localhost:7650/get-products";
   const APIResponse = await GET(url);
   if (APIResponse.error) {
-    return [];
+    return PRODUCTS;
   }
   if (APIResponse.data) {
     return APIResponse.data;
   }
-  return [];
+  return PRODUCTS;
 };
